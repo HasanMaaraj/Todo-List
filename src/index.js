6505de29166a19197b745d11ea4f507b5121bcaf
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-07-26 15:17:30 +0300

    Design a storage module to get, remove and add project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './style.css'
+const storage = (function() {
+    const getProjects = function() {
+        if (!localStorage.getItem('projects')) {
+            localStorage.setItem('projects', JSON.stringify({}))
+        }
+        return localStorage.getItem('projects');
+    };
+    
+    const saveProjects = function(projects) {
+        localStorage.setItem('projects', JSON.stringify(projects))
+    }
+
+    const addProject = function(title) {
+        let projects = getProjects();
+        projects[title] = [];
+        saveProjects(projects);
+    }
+
+    const removeProject = function(title) {
+        let projects = getProjects();
+        delete projects[title]
+        saveProjects(projects);
+    }
+
+    return {getProjects, addProject, removeProjects}
+})();
+
+document.querySelector('#add-project-form').addEventListener('submit', (e) => {
+    localStorage.getItem(projects)
+    e.preventDefault();
+    console.log(projects);
+});
