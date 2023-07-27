04a529e86b116aafe3c77f851145e238fa7e5d26
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-07-27 15:07:59 +0300

    Display task
    - Display a project's tasks when visiting a project
    - Add new added task to the tasks display

--- src/index.js
+++ src/index.js
@@ -44,11 +44,13 @@ const storage = (function() {
                 title,
                 date: new Date(date),
                 urgency,
-                description
+                description,
+                isComplete: false,
             }
         }
 
         const AddTask = function(project, title, date, urgency, description) {
+            // Create tas object and store in localStorage
             const task = taskFactory(title, date, urgency, description);
             const projects = storage.projectStorage.getProjects();
             projects[project].push(task);
@@ -56,11 +58,13 @@ const storage = (function() {
         }
 
         const getProjectsTasks = function(project) {
+            // Retrieve the project's tasks from the localStorage
             const projects = storage.projectStorage.getProjects();
             const tasks = projects[project]
             return tasks;
         }
 
+        return {AddTask, getProjectsTasks}
     })();
 
     
@@ -70,6 +74,48 @@ const storage = (function() {
 
 const display = (function(){
 
+    const tasksDisplay = (function(){
+
+        const appendTaskDiv = function(task) {
+            // Create a task div
+            const taskDiv = document.createElement('div');
+            taskDiv.className = 'task';
+            taskDiv.dataset.title = task.title;
+            // Create and append task title div
+            const taskTitle = document.createElement('div');
+            taskTitle.textContent = task.title;
+            taskTitle.className = 'task-title';
+            taskDiv.appendChild(taskTitle);
+            // Create and append task date div
+            const taskDate = document.createElement('div');
+            taskDate.textContent = task.date;
+            taskDate.className = 'task-date';
+            taskDiv.appendChild(taskDate);
+            // Create and append task urgency div
+            const taskUrgency = document.createElement('div');
+            taskUrgency.textContent = task.urgency;
+            taskUrgency.className = 'task-urgency';
+            taskDiv.appendChild(taskUrgency);
+            // Craete and append task descreption div
+            const taskDescription = document.createElement('div');
+            taskDescription.textContent = task.description;
+            taskDescription.className = 'task-description';
+            taskDiv.appendChild(taskDescription);
+            // append the task div to the .tasks-display div within the #project-display div
+            document.querySelector('.tasks-display').appendChild(taskDiv);
+        }
+
+        const displayProjectTasks = function(project) {
+            // Retrieve project's tasks and loop through them to append each task
+            const projectTasks = storage.taskStorage.getProjectsTasks(project);
+            projectTasks.forEach(task => {
+                appendTaskDiv(task)
+            });
+        }
+        return {displayProjectTasks, appendTaskDiv}
+    })();
+
+
     const projectsDisplay = (function() {
         const popProject = function(project) {
             // Delete chosen item from the list display
@@ -77,6 +123,7 @@ const display = (function(){
         }
 
         const clearProjectDisplay = function() {
+            // Clear the #project-display div
             const projectDisplay = document.querySelector('#project-display');
             Array.from(projectDisplay.childNodes).forEach(node => {
                 node.remove();
@@ -91,6 +138,12 @@ const display = (function(){
             projectHeader.className = 'project-header';
             document.querySelector('#new-task-project-name').value = project;
             projectDisplay.appendChild(projectHeader);
+            // Create a tasks display div for the tasks to be appended to it
+            const tasksDiv = document.createElement('div');
+            tasksDiv.className = 'tasks-display';
+            projectDisplay.appendChild(tasksDiv);
+            // Call a task display function to fill the tasks-display
+            tasksDisplay.displayProjectTasks(project)
         }
 
         const appendProject = function(project) {
@@ -133,14 +186,15 @@ const display = (function(){
         return {appendProject, displayProject};
     })();
 
-    return {projectsDisplay}
+    return {projectsDisplay, tasksDisplay}
 })();
 
 
 
 display.projectsDisplay.displayProject('My Project');
 
-document.querySelector('#add-project-form').addEventListener('submit', (e) => {
+const projectsForm = document.querySelector('#add-project-form');
+projectsForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const projectTitle = document.querySelector('#project-title').value;
     // Prevent the user from using the same name
@@ -149,4 +203,25 @@ document.querySelector('#add-project-form').addEventListener('submit', (e) => {
     storage.projectStorage.addProject(projectTitle);
     display.projectsDisplay.appendProject(projectTitle)
     document.querySelector('#project-title').value = '';
+});
+
+const tasksForm = document.querySelector('#add-task-form');
+tasksForm.addEventListener('submit', e => {
+    e.preventDefault();
+    // Obtain form data
+    const taskProject = document.getElementById('new-task-project-name').value;
+    const taskTitle = document.getElementById('task-title').value;
+    const taskDate = document.getElementById('due-date').value;
+    const taskUrgency = document.getElementById('urgency').value;
+    const taskDescription = document.getElementById('description').value;
+    // Add task to localStorage
+    storage.taskStorage.AddTask(taskProject, taskTitle, taskDate, taskUrgency, taskDescription);
+    // Add task to tasks-display
+    display.tasksDisplay.appendTaskDiv({
+        title: taskTitle,
+        date: new Date(taskDate),
+        urgency: taskUrgency,
+        description: taskDescription
+    })
+    console.log(taskTitle, taskDate, taskUrgency, taskDescription)
 });
